95ad9d119c28c2c1521674bbd87b55d21dbe50ad
gciruelos <gonzalo.ciruelos@gmail.com>
2015-04-18 04:17:27 -0300

    agregue ASM_merge1. La testie un par de veces y anda igual que la de C. La pueden usar de referencia para ver como funcionan los pixeles y las imagenes, perdi un buen tiempo intentando entenderlo :P. No tengo problema en hacer ASM_merge2

--- src/filters/ASM_merge1.asm
+++ src/filters/ASM_merge1.asm
@@ -15,15 +15,90 @@ ASM_merge1:
   push r14
   push r15
 
-  ; xmm0 = value
-  mov r12, rdi ; r12 = w
-  mov r13, rsi ; r13 = h
+ 
+               ; xmm0 = value
+  mov r12d, edi ; r12 = w
+  mov r13d, esi ; r13 = h
   mov r14, rdx ; r14 = data1
   mov r15, rcx ; r15 = data2
 
+  mov rax, r12 
+  mul r13      ; rax = h*w (suponiendo que no hay overflow
+  
+  mov rcx, 0   ; iterador
+  mov rbx, 0   ; posicion actual
+  
+  
+.loop:
+  cmp rcx, rax
+  je .fin
+  ; ABGR
+
+  movdqu xmm1, [r14 + rbx] ; xmm1 = [x|x|x|x|x|x|x|x | x|x|x|x|A|B|G|R]
+  movdqu xmm2, [r15 + rbx] ; xmm2 = [x|x|x|x|x|x|x|x | x|x|x|x|A|B|G|R]
+  pxor xmm3, xmm3
+
+  punpcklbw xmm1, xmm3     ; xmm1 =  [x|x|x|x|x|x|x|x | 0|A|0|B|0|G|0|R] 
+  punpcklbw xmm2, xmm3     ; xmm2 =  [x|x|x|x|x|x|x|x | 0|A|0|B|0|G|0|R] 
+
+  punpcklwd xmm1, xmm3     ; xmm1 =  [0|0|0|A|0|0|0|B | 0|0|0|G|0|0|0|R] 
+  punpcklwd xmm2, xmm3     ; xmm2 =  [0|0|0|A|0|0|0|B | 0|0|0|G|0|0|0|R] 
+
+  cvtdq2ps xmm1, xmm1      ; (float) xmm1
+  cvtdq2ps xmm2, xmm2      ; (float) xmm2
+
+  pxor xmm5, xmm5
+  movss xmm5, [_1]
+
+
+  pxor xmm3, xmm3          ; xmm3 =  0 | 0 | 0 | 0
+  movss xmm3, xmm0         ; xmm3 =  0 | 0 | 0 | value
+  pslldq xmm3, 4           ; xmm3 =  0 | 0 | value | 0
+  movss xmm3, xmm0         ; xmm3 =  0 | 0 | value | value
+  pslldq xmm3, 4           ; xmm3 =  0 | value | value | 0
+  movss xmm3, xmm0         ; xmm3 =  0 | value | value | value
+  pslldq xmm3, 4           ; xmm3 =  value | value | value | 0
+  addss xmm3, xmm5         ; xmm3 =  value | value | value | 1.0
+  
+  pxor xmm4, xmm4
+  addps xmm4, xmm5
+  pslldq xmm4, 4
+  addps xmm4, xmm5
+  pslldq xmm4, 4
+  addps xmm4, xmm5
+  pslldq xmm4, 4
+  addps xmm4, xmm5
+  pslldq xmm4, 4           ; xmm4 = 1.0 | 1.0 | 1.0 | 1.0
+
+  subps xmm4, xmm3         ; xmm4 = 1-value | 1-value | 1-value | 0.0
+  
+  mulps xmm1, xmm3         ; xmm1 = A_1 * 1.0 | B_1 * value | G_1 * value | R_1 * value   
+  mulps xmm2, xmm4         ; xmm2 = A_2 * 0.0 | B_2 * (1-value) | G_2 * (1-value) | R_2 * (1-value)
+
+  addps xmm1, xmm2         ; xmm1 = exactamente lo que queremos
+
+  cvtps2dq xmm1, xmm1      ; Convert Packed Single-Precision FP Values to Packed Dword Integers
+  
+  pxor xmm2, xmm2          ; xmm2 = 0
+  packusdw xmm1, xmm2      ; xmm1 = [0|0|0|0|0|0|0|0 | A|A|B|B|G|G|R|R]
+  packuswb xmm1, xmm2      ; xmm1 = [0|0|0|0|0|0|0|0 | 0|0|0|0|A|B|G|R]
+
+  movss [r14 + rbx], xmm1  ; lo guardo de nuevo en memoria
+
+  add rbx, 4
+  inc rcx
+
+  jmp .loop
+
+
+.fin:
   pop r15
   pop r14
   pop r13
   pop r12
   pop rbp
   ret
+
+_1: dd 1.0
+
+
