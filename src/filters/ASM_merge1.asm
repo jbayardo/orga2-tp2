d3da4edee53bd3ea9e428ca2a929c052f1d5ee62
gciruelos <gonzalo.ciruelos@gmail.com>
2015-04-18 18:28:27 -0300

    mejores comments a ASM_merge1.asm

--- src/filters/ASM_merge1.asm
+++ src/filters/ASM_merge1.asm
@@ -26,31 +26,10 @@ ASM_merge1:
   mul r13      ; rax = h*w (suponiendo que no hay overflow
   
   mov rcx, 0   ; iterador
-  mov rbx, 0   ; posicion actual
-  
-  
-.loop:
-  cmp rcx, rax
-  je .fin
-  ; BGRA
-
-  movdqu xmm1, [r14 + rbx] ; xmm1 = [x|x|x|x|x|x|x|x | x|x|x|x|B|G|R|A]
-  movdqu xmm2, [r15 + rbx] ; xmm2 = [x|x|x|x|x|x|x|x | x|x|x|x|B|G|R|A]
-  pxor xmm3, xmm3
-
-  punpcklbw xmm1, xmm3     ; xmm1 =  [x|x|x|x|x|x|x|x | 0|B|0|G|0|R|0|A] 
-  punpcklbw xmm2, xmm3     ; xmm2 =  [x|x|x|x|x|x|x|x | 0|B|0|G|0|R|0|A] 
-
-  punpcklwd xmm1, xmm3     ; xmm1 =  [0|0|0|B|0|0|0|G | 0|0|0|R|0|0|0|A] 
-  punpcklwd xmm2, xmm3     ; xmm2 =  [0|0|0|B|0|0|0|G | 0|0|0|R|0|0|0|A] 
-
-  cvtdq2ps xmm1, xmm1      ; (float) xmm1
-  cvtdq2ps xmm2, xmm2      ; (float) xmm2
-
-  pxor xmm5, xmm5
-  movss xmm5, [_1]
+  mov rbx, 0   ; posicion actual 
 
 
+  ;;;;;; Precalculo los vectores que voy a usar todos los loops
   pxor xmm3, xmm3          ; xmm3 =  0 | 0 | 0 | 0
   movss xmm3, xmm0         ; xmm3 =  0 | 0 | 0 | value
   pslldq xmm3, 4           ; xmm3 =  0 | 0 | value | 0
@@ -68,20 +47,41 @@ ASM_merge1:
   addps xmm4, xmm5
   pslldq xmm4, 4
   addps xmm4, xmm5
-  pslldq xmm4, 4           ; xmm4 = 1.0 | 1.0 | 1.0 | 1.0
+  pslldq xmm4, 4       ; xmm4 = 1.0 | 1.0 | 1.0 | 1.0
+  ;;;;;;;;
 
-  subps xmm4, xmm3         ; xmm4 = 1-value | 1-value | 1-value | 0.0
+.loop:
+  cmp rcx, rax   ; si iterador = h*w, listo, terminamos
+  je .fin
+
+  movdqu xmm1, [r14 + rbx] ; xmm1 = [x|x|x|x|x|x|x|x | x|x|x|x|B|G|R|A]
+  movdqu xmm2, [r15 + rbx] ; xmm2 = [x|x|x|x|x|x|x|x | x|x|x|x|B|G|R|A]
+  pxor xmm3, xmm3
+
+  punpcklbw xmm1, xmm3     ; xmm1 =  [x|x|x|x|x|x|x|x | 0|B|0|G|0|R|0|A] 
+  punpcklbw xmm2, xmm3     ; xmm2 =  [x|x|x|x|x|x|x|x | 0|B|0|G|0|R|0|A] 
+
+  punpcklwd xmm1, xmm3     ; xmm1 =  [0|0|0|B|0|0|0|G | 0|0|0|R|0|0|0|A] 
+  punpcklwd xmm2, xmm3     ; xmm2 =  [0|0|0|B|0|0|0|G | 0|0|0|R|0|0|0|A] 
+
+  cvtdq2ps xmm1, xmm1      ; (float) xmm1
+  cvtdq2ps xmm2, xmm2      ; (float) xmm2
+
+  pxor xmm5, xmm5
+  movss xmm5, [_1]
+
+  subps xmm4, xmm3     ; xmm4 = 1-value | 1-value | 1-value | 0.0
   
-  mulps xmm1, xmm3         ; xmm1 = A_1 * 1.0 | B_1 * value | G_1 * value | R_1 * value   
-  mulps xmm2, xmm4         ; xmm2 = A_2 * 0.0 | B_2 * (1-value) | G_2 * (1-value) | R_2 * (1-value)
+  mulps xmm1, xmm3     ; xmm1 = B1*value | G1*value | R1*value | A1*1.0   
+  mulps xmm2, xmm4     ; xmm2 = B2*(1-value) | G2*(1-value) | R2*(1-value)| A2*0
 
-  addps xmm1, xmm2         ; xmm1 = exactamente lo que queremos
+  addps xmm1, xmm2     ; xmm1 = exactamente lo que queremos
 
-  cvtps2dq xmm1, xmm1      ; Convert Packed Single-Precision FP Values to Packed Dword Integers
+  cvtps2dq xmm1, xmm1  ; Convert Packed Single-Precision FP Values to Packed Dword Integers
   
   pxor xmm2, xmm2          ; xmm2 = 0
-  packusdw xmm1, xmm2      ; xmm1 = [0|0|0|0|0|0|0|0 | A|A|B|B|G|G|R|R]
-  packuswb xmm1, xmm2      ; xmm1 = [0|0|0|0|0|0|0|0 | 0|0|0|0|A|B|G|R]
+  packusdw xmm1, xmm2      ; xmm1 = [0|0|0|0|0|0|0|0 | B|B|G|G|R|R|A|A]
+  packuswb xmm1, xmm2      ; xmm1 = [0|0|0|0|0|0|0|0 | 0|0|0|0|B|G|R|A]
 
   movss [r14 + rbx], xmm1  ; lo guardo de nuevo en memoria
 
@@ -89,7 +89,6 @@ ASM_merge1:
   inc rcx
 
   jmp .loop
-
 
 .fin:
   pop r15
