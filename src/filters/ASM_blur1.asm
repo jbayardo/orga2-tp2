0a4188f62133794f4e6cbb40c926200053a8bf21
Martin Baigorria <martinbaigorria@gmail.com>
2015-04-20 02:10:35 -0300

    Idea general, lo termino manana.

--- src/filters/ASM_blur1.asm
+++ src/filters/ASM_blur1.asm
@@ -5,142 +5,94 @@
 ;                                                                           ;
 ; ************************************************************************* ;
 
+% define SIZE_PIXEL 4
+
+; rax, rbx*, rcx, rdx, rsi, rdi, rbp, rsp, r8 ...  R12*, R13*, R14*, R15*
 ; void ASM_blur1( uint32_t w, uint32_t h, uint8_t* data )
 global ASM_blur1
 ASM_blur1:
+
   push rbp
   mov rbp, rsp
+  push rbx
   push r12
   push r13
   push r14
+  push r15
 
-  mov r12, rdi  ; r12 = w
-  mov r13, rsi  ; r13 = h
-  mov r14, rdx  ; r14 = data
+  mov rbx, rdi ; rbx: w (in pixels), pixeles por fila
+  mov r12, rsi ; r12: h (in pixels)
+  mov r13, rdx ; r13: pointer to data
 
-  mov rcx, 0x1  ; rcx = columna. Salteamos la primer columna porque es borde.
-  mov rdx, 0x1  ; rdx = fila. Salteamos la primer fila porque es borde.
+  ; reservar espacio para dos filas
+  shl rdi, 2   ; rdi*4
+  call malloc  ; rax: pointer to temp row 1
+  mov r14, rax ; 
 
-  .loop1:
-    ; Terminamos de recorrer columnas?
-    ; Salteamos la última porque es borde
-    cmp rcx, r13
-    je .end
+  mov rdi, rbx
+  shl rdi, 2   ; rdi*4, cada pixel tiene 4 bytes
+  call malloc  ; rax: pointer to temp row 0
+  mov r15, rax
 
-    .loop2:
-      ; Terminamos de recorrer filas?
-      ; Salteamos la última porque es borde
-      cmp rdx, r12
-      je .endloop2
+  ; variables
+  xor rdi, rdi ; contador filas recorridas
+  xor rdx, rdx ; contador columnas recorridas
 
-      pxor xmm15, xmm15
+  ; armar registro para dividir
+  pxor xmm2, xmm2 ; xmm2 = [9.0 | 9.0 | 9.0 | 9.0]
+  movss xmm2, [_9]
+  pslldq xmm2, 4
+  movss xmm2, [_9]
+  pslldq xmm2, 4
+  movss xmm2, [_9]
+  pslldq xmm2, 4
+  movss xmm2, [_9]
+  pslldq xmm2, 4
 
-      ; TODO: Arreglar este cansur de indexación, debuggear.
+  ; copiar primera fila
+.copy_row:
+  xor r8, r8   ; numero de pixeles en columnas recorridas
+  mov r9, rdi  ; copio numero de filas recorridas
+  mul r9, rbx  ; numero de pixeles en filas recorridas (actualizo fila)
 
-      movdqu xmm1, [r14 + r12 * (rdx - 1) + (rcx - 1)*4]
-      ; xmm1 = 3 pixeles de arriba, con basura en los primeros 4 bytes
-      ; xmm1 = [A|B|G|R | A|B|G|R | A|B|G|R | x|x|x|x]
-      psrldq xmm1, 8
-      ; xmm1 = [0|0|0|0 | A|B|G|R | A|B|G|R | A|B|G|R]
+.loop:
+  movdqu xmm5, [r13 + r9*SIZE_PIXEL + r8*SIZE_PIXEL] ; [data + row_index + col_offset]
+  movdqu [r14 + r8*SIZE_PIXEL], xmm5
+  add r8, 4   ; los pixeles son multiplos de 4
+  cmp r8, rbx
+  jne .loop   ; si son iguales, ya recorri todos
 
-      movdqu xmm2, [r14 + r12 * rdx + (rcx - 1)*4]
-      ; xmm2 = 3 pixeles de arriba, con basura en los primeros 4 bytes
-      ; xmm2 = [A|B|G|R | A|B|G|R | A|B|G|R | x|x|x|x]
-      psrldq xmm2, 8
-      ; xmm2 = [0|0|0|0 | A|B|G|R | A|B|G|R | A|B|G|R]
+  cmp rdi, 0  ; caso borde, donde al principio cargo la primera fila
+  jne .loop_columns:
 
-      movdqu xmm3, [r14 + r12 * (rdx + 1) + (rcx - 1)*4]
-      ; xmm2 = 3 pixeles de arriba, con basura en los primeros 4 bytes
-      ; xmm2 = [A|B|G|R | A|B|G|R | A|B|G|R | x|x|x|x]
-      psrldq xmm3, 8
-      ; xmm2 = [0|0|0|0 | A|B|G|R | A|B|G|R | A|B|G|R]
+.loop_rows:
+  inc rdi      ; paso a la siguiente fila
 
-      movdqu xmm4, xmm1
-      punpckhbw xmm4, xmm15
-      punpcklbw xmm1, xmm15
-      ; xmm1 = [0|A|0|B|0|G|0|R | 0|A|0|B|0|G|0|R]
-      ; xmm4 = [0|0|0|0|0|0|0|0 | 0|A|0|B|0|G|0|R] <- Columna 1 Fila 1
+  cmp rdi, r12 ; terminamos de iterar
+  je .fin
 
-      movdqu xmm5, xmm2
-      punpckhbw xmm5, xmm15
-      punpcklbw xmm2, xmm15
-      ; xmm2 = [0|A|0|B|0|G|0|R | 0|A|0|B|0|G|0|R]
-      ; xmm5 = [0|0|0|0|0|0|0|0 | 0|A|0|B|0|G|0|R] <- Columna 1 Fila 2
+  mov rsi, r14 ; m_tmp, copio puntero a la fila que recien copie
+  mov r15, r14 ; m_row_0 points to m_row_1 (la vieja que copie)
+  mov r14, rsi ; copiar nueva fila y mantener la anterior
+               ; puntero a fila anterior: r15, puntero a fila actual: r14
+  jmp .copy_row
 
-      movdqu xmm6, xmm3
-      punpckhbw xmm6, xmm15
-      punpcklbw xmm3, xmm15
-      ; xmm3 = [0|A|0|B|0|G|0|R | 0|A|0|B|0|G|0|R]
-      ; xmm6 = [0|0|0|0|0|0|0|0 | 0|A|0|B|0|G|0|R] <- Columna 1 Fila 3
+.loop_columns:
+  ;TODO
+  ;Iterar las filas que cree mas la siguiente
+  ;Convertir de ints de 8 bits a floats de 32 desempaquetando.
+  ;Dividir, luego sumar para evitar el overflow.
+  ;Empaquetar y escribir en *data el nuevo valor.
+  ;pasar a la siguiente fila con jmp .loop_rows
+  ;VER: Tenemos las filas 1 2 y 3 copiadas. Por ahi se puede optimizarr
+  ; para no tener que copiar esa fila denuevo.
 
-      paddw xmm1, xmm2
-      paddw xmm1, xmm3
-      ; xmm1 = xmm1 + xmm2 + xmm3
-      ; xmm1 = [A|A|B|B|G|G|R|R | A|A|B|B|G|G|R|R] <- Acumulado Columna 2 y 3
-
-      paddw xmm4, xmm5
-      paddw xmm4, xmm6
-      ; xmm4 = xmm4 + xmm5 + xmm6
-      ; xmm4 = [0|0|0|0|0|0|0|0 | A|A|B|B|G|G|R|R] <- Acumulado Columna 1
-
-      movdqu xmm2, xmm1
-      psrldq xmm2, 8
-      ; xmm2 = [0|0|0|0|0|0|0|0 | A|A|B|B|G|G|R|R] <- Acumulado Columna 2
-
-      pslldq xmm1, 8
-      psrldq xmm1, 8
-      ; xmm1 = [0|0|0|0|0|0|0|0 | A|A|B|B|G|G|R|R] <- Acumulado Columna 3
-
-      paddw xmm1, xmm2
-      paddw xmm1, xmm4
-      ; xmm1 = [0|0|0|0|0|0|0|0 | A|A|B|B|G|G|R|R] <- Suma total
-
-      ; OJO: No tenemos problemas de precisión porque la suma como mucho agrega
-      ; 1 bit por suma, y tenemos un total de 6 sumas, que es menos de 8 bits.
-
-      punpcklwd xmm1, xmm15
-      ; xmm1 = [0|0|A|A|0|0|B|B | 0|0|G|G|0|0|R|R]
-
-      cvtdq2pd xmm1, xmm1
-      ; xmm1 = "float[4](xmm1)"
-
-      pxor xmm2, xmm2
-      movss xmm2, [_9]
-      pslldq xmm2, 4
-      movss xmm2, [_9]
-      pslldq xmm2, 4
-      movss xmm2, [_9]
-      pslldq xmm2, 4
-      movss xmm2, [_9]
-      pslldq xmm2, 4
-      ; xmm2 = [9.0 | 9.0 | 9.0 | 9.0]
-
-      divps xmm1, xmm2
-      ; xmm1 = xmm1 / 9.0 en bache
-
-      cvtps2dq xmm1, xmm1
-      ; xmm1 = "uint16_t[4](xmm1)"
-
-      packusdw xmm1, xmm15
-      ; xmm1 = [0|0|0|0|0|0|0|0 | A|A|B|B|G|G|R|R] (saturado)
-
-      packuswb xmm1, xmm15
-      ; xmm1 = [0|0|0|0|0|0|0|0 | 0|0|0|0|A|B|G|R] (saturado)
-
-      ; Guardar en memoria el valor que acabamos de procesar
-      movss [r14 + r12 * rdx + rcx*4], xmm1
-
-      inc rdx
-      jmp loop2
-
-  .endloop2:
-    inc rcx
-    jmp loop1
-
-.end:
+.fin:
+  pop r15
   pop r14
   pop r13
   pop r12
+  pop rbx
   pop rbp
   ret
 
